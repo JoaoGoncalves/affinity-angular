df40585ab5a2ac3f4835939d7d54e53cbf563f69
Joao Goncalves <joao@joaogoncalves.net>
2025-09-09 21:10:59 +0100

    sessao 1

---
+++ hrms/src/app/pages/login.ts
@@ -0,0 +1,47 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, NgIf],
+  providers: [AuthService],
+  template: `
+    <div class="login-container">
+      <h1>Login</h1>
+      <form>
+        <input type="text" name="email" placeholder="Email" [(ngModel)]="credentials.email" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          [(ngModel)]="credentials.password"
+        />
+        <button type="submit" (click)="submit()">Login</button>
+      </form>
+      <span class="warning" *ngIf="!credentials.email || !credentials.password">
+        Please fill in all the required fields
+      </span>
+      <!-- @if (!credentials.email || !credentials.password) {
+      <span class="warning"> Please fill in all the required fields </span>
+      } -->
+    </div>
+  `,
+  styles: ``,
+})
+export class Login {
+  credentials = { email: '', password: '' };
+
+  constructor(private authService: AuthService){}
+
+  submit(){
+    if(this.credentials.email && this.credentials.password){
+
+      console.log('Email:', this.credentials.email);
+      console.log('Password:', this.credentials.password);
+
+      this.authService.login(this.credentials).subscribe();
+    }
+  }
+}
